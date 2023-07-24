ced48ffadd70c3134ab626ca8c6a46e700684c18
sagorranait <sagorranait@gmail.com>
2023-07-24 21:12:26 +0600

    The Design of Tab Navigation is done.

--- Tabs.js
+++ Tabs.js
@@ -2,9 +2,11 @@ import Home from './screens/Home';
 import Likes from './screens/Likes';
 import AddNew from './screens/AddNew';
 import Profile from './screens/Profile';
+import { StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/AntDesign';
+import { COLORS } from './constants';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,13 +15,20 @@ const Tabs = () => {
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="home"
+        screenOptions={()=>({
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: styles.tabBarStyle,
+          tabBarActiveTintColor: COLORS.white,
+          tabBarInactiveTintColor: COLORS.dark,
+        })}
       >
         <Tab.Screen 
           name="home" 
           component={Home} 
           options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="home" color={color} size={size} />
+            tabBarIcon: ({ color, size, focused }) => (
+              <Icon name="home" color={color} size={size} style={focused && styles.tabBarIcon} />
             ),
           }}
         />
@@ -27,8 +36,8 @@ const Tabs = () => {
           name="likes" 
           component={Likes} 
           options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="heart" color={color} size={size} />
+            tabBarIcon: ({ color, size, focused }) => (
+              <Icon name="heart" color={color} size={size} style={focused && styles.tabBarIcon} />
             ),
           }}
         />
@@ -36,8 +45,8 @@ const Tabs = () => {
           name="AddNew" 
           component={AddNew} 
           options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="plus" color={color} size={size} />
+            tabBarIcon: ({ color, size, focused }) => (
+              <Icon name="plus" color={color} size={size} style={focused && styles.tabBarIcon} />
             ),
           }}
         />
@@ -45,8 +54,8 @@ const Tabs = () => {
           name="profile" 
           component={Profile} 
           options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="user" color={color} size={size} />
+            tabBarIcon: ({ color, size, focused }) => (
+              <Icon name="user" color={color} size={size} style={focused && styles.tabBarIcon} />
             ),
           }}
         />
@@ -55,4 +64,31 @@ const Tabs = () => {
   )
 }
 
-export default Tabs
+export default Tabs;
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    position: 'absolute',
+    borderTopWidth: 0,
+    bottom: 15,
+    right: 10,
+    left: 10,
+    elevation: 0,
+    backgroundColor: COLORS.whiteTransparent,
+    borderRadius: 10,
+    height: 75,
+  },
+  tabBarIcon: { 
+    backgroundColor: COLORS.primary, 
+    padding: 12, 
+    borderRadius: 50,
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  }
+});
---
+++ constants/colors.js
@@ -0,0 +1,7 @@
+export default {
+   primary: '#e60023',
+   secondary: '#ad081b',
+   white: '#FFFFFF',
+   black: '#0a0a0a',
+   whiteTransparent: '#ffffff24',
+ };
