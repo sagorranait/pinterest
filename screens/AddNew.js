b9d888ccce7bc305bc1a21f032bc80571429bfda
sagorranait <sagorranait@gmail.com>
2023-07-29 16:04:46 +0600

    Design of Add New Screens is done.

--- screens/AddNew.js
+++ screens/AddNew.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { 
   Text, 
   View, 
   Alert,
-  Pressable,
-  StyleSheet, 
+  Image, 
+  TextInput,
+  StyleSheet,
+  ScrollView,
 } from 'react-native';
 import { COLORS } from '../constants/index';
 import * as ImagePicker from 'expo-image-picker';
@@ -13,43 +15,93 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import CustomButton from '../components/CustomButton';
 
 export default AddNew = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [ratio, setRatio] = useState(1);
+  const [pinData, setPinData] = useState({
+		image: '',
+		title: '',
+		description: ''
+	});
 
-  const savePinHandler = () => {
-
-  }
+  useEffect(() => {
+    if (pinData.image) {
+      Image.getSize(pinData.image, (width, height) => setRatio(width / height));
+    }
+  }, [pinData.image]);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       quality: 1,
     });
 
     if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
+      setPinData((preData) => {
+        return { ...preData, 'image': result.assets[0].uri }
+      });
     } else {
       Alert.alert('You did not select any image.');
     }
   };
 
+  const inputHandler = (identifier, enteredText) => {
+    setPinData((currentText)=> {
+			return {
+        ...currentText,
+			  [identifier]: enteredText
+      }
+		});
+  }
+
+  const savePinHandler = () => {
+
+  }
+
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <CustomButton label='Create Pin' outline={false} position='flex-end' />
-        <View style={styles.formArea}>
-          <View style={styles.imageArea}>
-            <Icon name="image" color={COLORS.gray} size={100} />
-            <Text style={styles.imageText} >Image Placeholder</Text>
+      <ScrollView>
+        <View style={styles.container}>
+          <CustomButton label='Create Pin' outline={false} position='flex-end' onPress={savePinHandler}  />
+          <View style={styles.formArea}>
+            <View style={[styles.imageArea, { borderWidth: pinData.image ? 0 : 1 }]}>
+              { pinData.image ? 
+              <Image source={{ uri: pinData.image }} style={[styles.image, {aspectRatio: ratio}]} /> :
+              <>              
+                <Icon name="image" color={COLORS.gray} size={100} />
+                <Text style={styles.imageText} >Image Placeholder</Text>
+              </>
+              }
+              <CustomButton 
+                label={pinData.image ? 'Choose Another' : 'Select An Image'} 
+                icon='upload-to-cloud'
+                outline={true}
+                onPress={pickImageAsync}
+                position='center'
+              />
+            </View>
+            <View style={styles.inputArea}>
+              <View style={styles.input}>
+                <Text style={styles.inputTitle}>Title</Text>
+                <TextInput
+                  value={pinData.title}
+                  style={styles.textInput}
+                  placeholder='Give your pin a title.'
+                  onChange={inputHandler.bind(this, 'title')}
+                />
+              </View>
+              <View style={styles.input}>
+                <Text style={styles.inputTitle}>Description</Text>
+                <TextInput
+                  value={pinData.title}
+                  style={styles.textInput}
+                  placeholder='Tell everyone what your Pin about.'
+                  onChange={inputHandler.bind(this, 'title')}
+                />
+              </View>
+            </View>
           </View>
-          <CustomButton 
-            label='Select An Image' 
-            icon='upload-to-cloud'
-            outline={true}
-            onPress={pickImageAsync}
-            position='center'
-          />
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
@@ -64,16 +116,37 @@ const styles = StyleSheet.create({
   imageArea: {
     width: '100%',
     minHeight: 280,
-    borderWidth: 1,
     borderRadius: 15,
     alignItems: 'center',
     justifyContent: 'center',
     borderColor: COLORS.gray,
+  },
+  image: {
+    width: '100%',
+    borderRadius: 15,
   },
   imageText: {
     fontSize: 17,
     fontWeight: '600',
     color: COLORS.gray,
     margin: 15,
-  }
+  },
+  inputArea: {
+  },
+  inputTitle: {
+    fontSize: 18,
+    marginTop: 15,
+    paddingLeft: 7,
+    fontWeight: '600',
+    paddingBottom: 10,
+    color: COLORS.black,
+  },
+  textInput: {
+    fontSize: 16,
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: COLORS.gray,
+  },
 });
