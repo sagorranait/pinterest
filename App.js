f73ca4ecb7b516f7805d0d96b30db6932f4ec9c8
sagorranait <sagorranait@gmail.com>
2023-08-19 22:22:47 +0600

    Setup nhost in the project

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import * as SecureStore from "expo-secure-store";
+import { NhostClient, NhostProvider } from '@nhost/react';
+
 import Navigation from './navigation';
+
+const nhost = new NhostClient({
+  subdomain: 'ibcqjvxouabmgbluekcc',
+  region: 'ap-south-1',
+  clientStorageType: "expo-secure-storage",
+	clientStorage: SecureStore,
+})
 
 export default App = () => {
   return (
-    <>
+    <NhostProvider nhost={nhost}>
       <Navigation/>
       <StatusBar style="auto" />
-    </>
+    </NhostProvider>
   );
 }
