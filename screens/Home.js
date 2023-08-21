6b2030ed3145fa1ef2c6b81e7d09446a541b0020
sagorranait <sagorranait@gmail.com>
2023-08-21 16:30:33 +0600

    Getting All Pins data and By Id are done.

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,38 @@
-import { ScrollView } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, ScrollView } from 'react-native';
+import { useNhostClient } from '@nhost/react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import pins from '../assets/data/pins';
 import MasonryList from '../components/MasonryList';
 
 export default Home = () => {
+  const nhost = useNhostClient();
+  const [pins, setPins] = useState([]);
+
+  const fetchPins = async () => {
+    const response = await nhost.graphql.request(`
+      query MyQuery {
+        pins {
+          id
+          image
+          title
+        }
+      }
+    `);
+
+    if (response.error) {
+      Alert.alert('Error Fetching Pins !!!');
+    }else{
+      setPins(response?.data?.pins || []);
+    }
+  }
+
+  useEffect(() => {
+    fetchPins();
+  }, [])
+  
+
   return (
     <SafeAreaView>
       <ScrollView>
--- screens/SinglePin.js
+++ screens/SinglePin.js
@@ -6,39 +6,66 @@ import {
    Image, 
    Pressable,
    StyleSheet,
+   Alert,
 } from 'react-native';
-
-import pins from '../assets/data/pins';
 import { COLORS } from '../constants';
+import { useNhostClient } from '@nhost/react';
 import Icon from 'react-native-vector-icons/AntDesign';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default SinglePin = () => {
-  const route = useRoute();
-  const { id } = route.params;
+  const nhost = useNhostClient();
   const navigation = useNavigation();
   const insets = useSafeAreaInsets();
+
+  const route = useRoute();
+  const { id } = route.params;
+
   const [ratio, setRatio] = useState(1);
+  const [pin, setPin] = useState(null);
   
-  const pin = pins.find(pin => pin.id === id);
+  const fetchPins = async () => {
+   const response = await nhost.graphql.request(`
+     query PinById {
+         pins_by_pk(id: "${id}") {
+            id
+            title
+            image
+            description
+         }
+      }
+   `);
+
+   if (response.error) {
+     Alert.alert('Error Fetching Pins !!!');
+   }else{
+     setPin(response?.data?.pins_by_pk || {});
+   }
+ }
+
+   useEffect(() => {
+      fetchPins();
+   }, [])
 
   useEffect(() => {
-    if (pin.image) {
-      Image.getSize(pin.image, (width, height) => setRatio(width / height));
+    if (pin?.image) {
+      Image.getSize(pin?.image, (width, height) => setRatio(width / height));
     }
-  }, [pin.image]);
+  }, [pin?.image]);
 
   const goBackHandler = () => {
    navigation.goBack();
   }
 
+  
+
   return (
    <SafeAreaView style={styles.root}>
       <StatusBar style='light'/>
       <View style={styles.container}>
-         <Image style={[styles.image, {aspectRatio: ratio}]} source={{ uri: pin.image }} />
-         <Text style={styles.title}>{pin.title}</Text>
+         <Image style={[styles.image, {aspectRatio: ratio}]} source={{ uri: pin?.image }} />
+         <Text style={styles.title}>{pin?.title}</Text>
       </View>
       <Pressable onPress={goBackHandler} style={[styles.backBtn, { top: insets.top + 25 }]} >
          <Icon name='leftcircleo' size={30} color={COLORS.white} />
